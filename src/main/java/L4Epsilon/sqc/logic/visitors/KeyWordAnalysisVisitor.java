988607fc35c01165d2714d84450e6286147a14ca
gruszek-i <plgrucha@gmail.com>
2024-12-12 14:16:28 +0100

    Dodano zliczanie slow kluczowych

--- src/main/java/L4Epsilon/sqc/logic/visitors/KeyWordAnalysisVisitor.java
+++ src/main/java/L4Epsilon/sqc/logic/visitors/KeyWordAnalysisVisitor.java
@@ -2,10 +2,20 @@ package L4Epsilon.sqc.logic.visitors;
 
 import L4Epsilon.sqc.logic.elements.*;
 
+import java.util.List;
+
 public class KeyWordAnalysisVisitor implements Visitor{
     private int occurrenceCount;
 
-    public int getOccurrenceCount(){
+    public int getOccurrenceCount(Scenario scenario){
+        int occurrenceCount = 0;
+        for (Step step : scenario.getSteps()) {
+            for (Action actionx : step.getActions()){
+                if (actionx.getText().contains("IF") || actionx.getText().contains("FOR EACH") || actionx.getText().contains("ELSE")){
+                    occurrenceCount ++;
+                }
+            }
+        }
         return occurrenceCount;
     }
 
