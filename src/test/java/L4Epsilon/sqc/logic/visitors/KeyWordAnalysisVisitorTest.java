954bff39ebab55cab9c2be826e4e89ab1086995b
miiraai <mikcerog@gmail.com>
2025-01-26 16:46:45 +0100

    dodanie KeyWordAnalysisVisitorTest.java

--- src/test/java/L4Epsilon/sqc/logic/visitors/KeyWordAnalysisVisitorTest.java
+++ src/test/java/L4Epsilon/sqc/logic/visitors/KeyWordAnalysisVisitorTest.java
@@ -1,6 +1,7 @@
 package L4Epsilon.sqc.logic.visitors;
 
 import L4Epsilon.sqc.logic.elements.Action;
+import L4Epsilon.sqc.logic.elements.Instruction;
 import L4Epsilon.sqc.logic.elements.Scenario;
 import L4Epsilon.sqc.logic.elements.Step;
 import org.junit.jupiter.api.Test;
@@ -10,18 +11,79 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class KeyWordAnalysisVisitorTest {
 
+    int occurence = 0;
 
     @Test
     void test_IF() {
+        String text = "IF";
+
+        List<Instruction> instructions = new ArrayList<>();
+
+        Step step = new Step(instructions, text);
+
+        KeyWordAnalysisVisitor visitor = new KeyWordAnalysisVisitor();
+        visitor.visitStep(step);
+
+        assertEquals(1, visitor.getOccurrenceCount());
     }
+
 
     @Test
     void test_ELSE() {
+        String text = "ELSE";
+
+        List<Instruction> instructions = new ArrayList<>();
+
+        Step step = new Step(instructions, text);
+
+        KeyWordAnalysisVisitor visitor = new KeyWordAnalysisVisitor();
+        visitor.visitStep(step);
+
+        assertEquals(1, visitor.getOccurrenceCount());
     }
 
     @Test
     void test_FOR_EACH() {
+        String text = "FOR EACH";
+
+        List<Instruction> instructions = new ArrayList<>();
+
+        Step step = new Step(instructions, text);
+
+        KeyWordAnalysisVisitor visitor = new KeyWordAnalysisVisitor();
+        visitor.visitStep(step);
+
+        assertEquals(1, visitor.getOccurrenceCount());
+    }
+
+    @Test
+    void test_FOR_IF_EACH() {
+        String text = "FOR IF EACH";
+
+        List<Instruction> instructions = new ArrayList<>();
+
+        Step step = new Step(instructions, text);
+
+        KeyWordAnalysisVisitor visitor = new KeyWordAnalysisVisitor();
+        visitor.visitStep(step);
+
+        assertEquals(0, visitor.getOccurrenceCount());
+    }
+
+    @Test
+    void test_ELSE_IF() {
+        String text = "ELSE IF";
+
+        List<Instruction> instructions = new ArrayList<>();
+
+        Step step = new Step(instructions, text);
+
+        KeyWordAnalysisVisitor visitor = new KeyWordAnalysisVisitor();
+        visitor.visitStep(step);
+
+        assertEquals(1, visitor.getOccurrenceCount());
     }
 }
